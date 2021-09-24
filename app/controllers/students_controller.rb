d62720ee8240ae61f7df6b78472c884003f74bc4
Andrew McIndoe <amcindoe@powerhrg.com>
2021-09-24 09:26:04 -0400

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,25 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new; end
+
+  def create
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to students_path
+  end
+
 end
+
+
+#for show can also do below
+#class 
+#StudentsController < ApplicationController
+#before_action :set_student, only: :show
+
+#def show; end
+
+# private
+
+#     def set_student
+#       @student = Student.find(params[:id])
+#     end
